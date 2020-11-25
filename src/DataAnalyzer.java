f36923cfa385e596a4c7355bd05cd52b400f7d6d
vanya <vanyachop@gmail.com>
2020-11-25 23:42:41 +0100

    Input data verification by RegEx was added.

--- src/DataAnalyzer.java
+++ src/DataAnalyzer.java
@@ -2,13 +2,15 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DataAnalyzer {
     private List<WaitingTimeline> waitingTimelineList = new ArrayList<>();
 
     static {
         File file = new File("src/output.txt");
-        if( file.exists()) {
+        if (file.exists()) {
             file.delete();
         }
     }
@@ -16,20 +18,35 @@ public class DataAnalyzer {
     public void analyze() {
         String[] lines = readFromFile();
         Arrays.stream(lines).forEach(l -> {
-            if (l.charAt(0) == 'C') {
-                WaitingTimeline waitingTimeline = new WaitingTimeline(l.substring(2, l.length()));
-                waitingTimelineList.add(waitingTimeline);
-            } else if (l.charAt(0) == 'D') {
-                QueryLine queryLine = new QueryLine(l.substring(2, l.length()));
-                selectMatches(queryLine);
+            if (matchesPattern(l)) {
+                if (l.charAt(0) == 'C') {
+                    WaitingTimeline waitingTimeline = new WaitingTimeline(l.substring(2, l.length()));
+                    waitingTimelineList.add(waitingTimeline);
+                } else if (l.charAt(0) == 'D') {
+                    QueryLine queryLine = new QueryLine(l.substring(2, l.length()));
+                    selectMatches(queryLine);
+                }
             }
         });
+    }
+
+    public boolean matchesPattern(String line) {
+        Pattern pattern = Pattern.compile("[CD]\\s" +
+                "((([1-9]|10)([.][1-3])?)|[*])\\s" +
+                "((([1-9]|10)([.]([1-9]|([1][0-9]|20))([.][1-5])?)?)|[*])\\s" +
+                "[PN]\\s" +
+                "(3[01]|[12][0-9]|0?[1-9])\\.(1[012]|0?[1-9])\\.((?:19|20)\\d{2})" +
+                "([-](3[01]|[12][0-9]|0?[1-9])\\.(1[012]|0?[1-9])\\.((?:19|20)\\d{2}))?" +
+                "(\\s\\d*)?"
+        );
+        Matcher m = pattern.matcher(line);
+        return m.matches();
     }
 
     private void selectMatches(QueryLine queryLine) {
         List<WaitingTimeline> matchedWaitingTimelines = new ArrayList<>();
         waitingTimelineList.forEach(e -> {
-            if(queryLine.isMatch(e)) {
+            if (queryLine.isMatch(e)) {
                 matchedWaitingTimelines.add(e);
             }
         });
@@ -42,9 +59,8 @@ public class DataAnalyzer {
         int matchedElementsSum = matchedWaitingTimelines.stream().mapToInt(e -> e.time).sum();
         long matchedElementsCount = matchedWaitingTimelines.size();
         if (matchedElementsCount != 0) {
-            return Long.toString(matchedElementsSum/matchedElementsCount);
-        }
-        else return "-";
+            return Long.toString(matchedElementsSum / matchedElementsCount);
+        } else return "-";
     }
 
     private void writeToFile(String string) {
@@ -73,10 +89,9 @@ public class DataAnalyzer {
                 line = reader.readLine();
             }
             return lines;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-            catch(IOException e){
-                e.printStackTrace();
-            }
         return null;
-        }
     }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         DataAnalyzer dataAnalyzer = new DataAnalyzer();
         dataAnalyzer.analyze();
+
+//        Pattern pattern = Pattern.compile("[CD]\\s" +
+//                "((([1-9]|10)([.][1-3])?)|[*])\\s" +
+//                "((([1-9]|10)([.]([1-9]|([1][0-9]|20))([.][1-5])?)?)|[*])\\s" +
+//                "[PN]\\s" +
+//                "(3[01]|[12][0-9]|0?[1-9])\\.(1[012]|0?[1-9])\\.((?:19|20)\\d{2})" +
+//                "([-](3[01]|[12][0-9]|0?[1-9])\\.(1[012]|0?[1-9])\\.((?:19|20)\\d{2}))?" +
+//                "(\\s\\d*)?"
+//        );
+//        Matcher m = pattern.matcher("D 1 * P ");
+//        System.out.println(m.matches());
     }
 }
